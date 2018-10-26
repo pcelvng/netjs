6745b60857b96e70f4f63ac2eb9c45ab96535cfb
Ryan Ball <ryan.ball@getweave.com>
2018-10-26 11:15:39 -0600

    err on localnetwork callback

--- examples/localnetwork.js
+++ examples/localnetwork.js
@@ -1,11 +1,14 @@
 const net = require("../net.js");
-const util = require('util');
 
 // generate all localnetwork objects including
 // and especially the core local network hosts.
-net.localNetwork((hsts) => {
+net.localNetwork((lnErr, hsts) => {
+    if (lnErr) {
+        console.log("public host error:");
+        console.log(lnErr.toString());
+    }
+
     for (let i = 0; i < hsts.length; i++) {
-        // console.log(util.inspect(hsts[i], false, null, true));
         console.log(JSON.stringify(hsts[i]));
     }
 });
